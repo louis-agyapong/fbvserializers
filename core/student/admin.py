6863532e7645bf1197d15bb0adc05aed0643680c
Louis Agyapong <agyapong.louis@gmail.com>
2022-05-23 17:06:51 +0000

    initial commit

---
+++ core/student/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Student
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ["name", "age", "email", "score", "created_at", "updated_at"]
+    list_filter = ["created_at", "updated_at"]
+    search_fields = ["name", "email"]
+    ordering = ["-created_at"]
---
+++ core/student/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    email = models.EmailField()
+    score = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        db_table = "student"
+        verbose_name = "Student"
+        verbose_name_plural = "Students"
+        ordering = ["-created_at"]
